8c6afcecc1ecc7e38ef8183abb91e8ae7f71e6f5
MaksAnikeev <anikeev.maks@rambler.ru>
2023-08-21 17:09:41 +0300

    Update examples.py

--- examples.py
+++ examples.py
@@ -15,24 +15,24 @@ from pprint import pprint
 # print(response.json())
 
 
-# '''Получаем данные по датам с выбором поля для упорядочивания вывода'''
-# url = 'http://127.0.0.1:8000/statistics/get'
-# payload = {
-#     'date_from': '2023-08-01',
-#     'date_to': '2023-08-21',
-#     'order': 'clicks'
-# }
-# response = requests.post(url, data=payload)
-# response.raise_for_status()
-# pprint(response.json())
-
-'''Очищаем статистику'''
-url = 'http://127.0.0.1:8000/statistics/clear'
+'''Получаем данные по датам с выбором поля для упорядочивания вывода'''
+url = 'http://127.0.0.1:8000/statistics/get'
 payload = {
-    'date_from': '2023-08-19',
+    'date_from': '2023-07-01',
     'date_to': '2023-08-21',
-    'clear_all': None
+    'order': 'clicks'
 }
 response = requests.post(url, data=payload)
 response.raise_for_status()
 pprint(response.json())
+
+# '''Очищаем статистику'''
+# url = 'http://127.0.0.1:8000/statistics/clear'
+# payload = {
+#     'date_from': '2023-08-19',
+#     'date_to': '2023-08-21',
+#     'clear_all': None
+# }
+# response = requests.post(url, data=payload)
+# response.raise_for_status()
+# pprint(response.json())
